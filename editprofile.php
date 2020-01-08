5b5549a7fd698cc7482df4024d4703f771c03abf
Dusan Martinovic <dusan.martinovic.fit@gmail.com>
2020-01-09 00:49:13 +0100

    del comm

--- editprofile.php
+++ editprofile.php
@@ -110,42 +110,6 @@ $data = ob_get_clean();
    header("location: index.php");
   }
 
-  /*if(empty($_FILES['avatar']['temp_name'])===false){
-      $file_ext= end(explode('.',$_FILES['avatar']['name']));
-      if(in_array(strtolower($file_ext),array('jpg','jpeg','png','gif'))===false){
-        $errors[]='You avatar must be an image.';
-      }
-      return true;
-
-  }*/
-
-/*function setImg($avatar){
-    if(file_exists($avatar)){
-      $src_size=getImagesize($avatar);
-      if($src_size['mime']==='image/jpeg'){
-          $src_img = Imagecreatefromjpeg($avatar);
-      }else if($src_size['mime']==='image/png'){
-          $src_img = Imagecreatefrompng($avatar);
-      }else if($src_size['mime']==='image/gif'){
-          $src_img = Imagecreatefromgif($avatar);
-      }else{
-          $src_img=false;
-      }
-      if($src_img!==false){
-          $thumb_width= 200;
-          if($src_size[0]<= $thumb_width){
-            $thumb=$src_img;
-          }else{
-            $new_size[0]=$thumb_width;
-            $new_size[1]=($src_size[1]/$src_size[0])*$thumb_width;
-            $thumb= imagecreatetruecolor($new_size[0],$new_size[1]);
-            imagecopyresampled($thumb,$src_img,0,0,0,0,$new_size[0],$new_size[1],$src_size[0],$src_size[1]);
-
-          }
-          imagejpeg($thumb,"{$GLOBALS['path']}/avatars/{$_SESSION['username']}.jpg");
-      }
-    }
-  }*/
   function getEditInfo($s,$id){
 	 require('db/db.php');
 	  
